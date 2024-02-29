f32a903b1612dce8c892cc0bb0b6411a4ed63323
ThishanJude <thishan1997@gmail.com>
2024-02-29 11:39:53 +0530

    Added some functions to logic of the password generating

--- App.tsx
+++ App.tsx
@@ -21,15 +21,49 @@ function App() {
 
   const generatePasswordString = (passwordLength: number) => {
     //Generating password string
+    let characterList = '';
+
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+';
+
+    if (upperCase) {
+      characterList += upperCase;
+    }
+    if (lowerCase) {
+      characterList += lowerCase;
+    }
+    if (numbers) {
+      characterList += numbers;
+    }
+    if (symbols) {
+      characterList += symbols;
+    }
+
+    const passwordResult = createPassword(characterList, passwordLength);
+    setPassword(passwordResult);
+    setIsPassGenerated(true);
   };
 
   const createPassword = (characters: string, passwordLength: number) => {
     //create password
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
   };
 
   const resetPasswordState = () => {
-  //reset password
-  }
+    //reset password
+    setPassword('');
+    setUpperCase(false);
+    setLowerCase(false);
+    setNumbers(false);
+    setSymbols(false);
+  };
 
   return (
     <SafeAreaView>
